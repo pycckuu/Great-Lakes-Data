d2ccafd825e26a99c1481edcd7fd00641785cab9
pycckuu <is.markelov@gmail.com>
2017-02-13 17:30:08 -0500

    updated plotting scripts and data

--- post_proc_scripts/ploting.py
+++ post_proc_scripts/ploting.py
@@ -1,26 +1,48 @@
 import matplotlib.pyplot as plt
-import matplotlib
+# import matplotlib
 import pandas as pd
-from datetime import datetime
+# from datetime import datetime
 from windrose import WindroseAxes
 import numpy as np
 # import statsmodels.api as sm
 from statsmodels.sandbox.regression.predstd import wls_prediction_std
 import statsmodels.formula.api as sm
+import glob
 import os
 
 import seaborn as sns
-from matplotlib.colors import ListedColormap
+# from matplotlib.colors import ListedColormap
 sns.set_style("whitegrid")
 
-save_fig = False
+
+SAVE_FIG = True
+# SAVE_FIG = True
+DPI = 150
 
 
 def linear_fit(df, column):
+    """Summary
+
+    Args:
+        df (TYPE): Description
+        column (TYPE): Description
+
+    Returns:
+        TYPE: Description
+    """
     return sm.ols(formula=column + " ~ num", data=df).fit()
 
 
 def graph_size(h, v):
+    """Summary
+
+    Args:
+        h (TYPE): Description
+        v (TYPE): Description
+
+    Returns:
+        TYPE: Description
+    """
     plt.rcParams['figure.figsize'] = h, v
     font = {'family': 'serif',
             'weight': 'bold',
@@ -31,6 +53,19 @@ def graph_size(h, v):
 
 
 def plot_graphs(df, column, lgnd, units, lines=True, style='-'):
+    """Summary
+
+    Args:
+        df (TYPE): Description
+        column (TYPE): Description
+        lgnd (TYPE): Description
+        units (TYPE): Description
+        lines (bool, optional): Description
+        style (str, optional): Description
+
+    Returns:
+        TYPE: Description
+    """
     df["TMP"] = df.index.values                # index is a DateTimeIndex
     df = df[df.TMP.notnull()]                  # remove all NaT values
     df.drop(["TMP"], axis=1, inplace=True)
@@ -64,22 +99,41 @@ def plot_graphs(df, column, lgnd, units, lines=True, style='-'):
     # ax.axis([0, 10, 0, 10])
     # plt.plot(x, iv_u.values, 'r--')
     # plt.plot(x, iv_l.values, 'r--')
-    if save_fig:
+    if SAVE_FIG:
         plt.savefig('plots/input/western/' + lgnd + '.png', dpi=150)
     plt.show()
 
 
 def plot_windrose(df):
+    """Summary
+
+    Args:
+        df (TYPE): Description
+
+    Returns:
+        TYPE: Description
+    """
     ax = WindroseAxes.from_ax()
     ax.box(df['WDIR'], df['WSPD'], bins=np.arange(0, 16, 3))
     # ax.bar(df['WDIR'], df['WSPD'], normed=True, opening=0.8, edgecolor='white')
     ax.set_legend()
-    if save_fig:
+    if SAVE_FIG:
         plt.savefig('plots/input/western/windrose.png', dpi=150)
     plt.show()
 
 
 def load_data(pth, parse_dates=[[0, 1, 2]], skiprows=None, encoding="utf8"):
+    """Loads the data from csv files
+
+    Args:
+        pth (TYPE): path to file
+        parse_dates (list, optional): columns of dates
+        skiprows (None, optional): how many rows to skip
+        encoding (str, optional): encoding
+
+    Returns:
+        TYPE: returns pandas dataframe
+    """
     df = pd.read_csv(pth, parse_dates=parse_dates, skiprows=skiprows, encoding=encoding)
     df.rename(columns={df.columns[0]: 'YY_MM_DD'}, inplace=True)
     # df['YY_MM_DD'] = df[['YEAR', 'MONTH', 'DAY']].apply(lambda s : datetime.datetime(*s),axis = 1)
@@ -92,92 +146,12 @@ def load_data(pth, parse_dates=[[0, 1, 2]], skiprows=None, encoding="utf8"):
     df["TMP"] = df.index.values                # index is a DateTimeIndex
     df = df[df.TMP.notnull()]                  # remove all NaT values
     df.drop(["TMP"], axis=1, inplace=True)
+    df = df.sort_index()
     return df
 
 
-def plotting_weather_western_basin():
-    df = load_data('../measurements/weather/National_Buoy_Data_Center/basin_average/western_basin_average.csv')
-    plot_graphs(df, 'ATMP', 'Temperature', 'C')
-    plot_graphs(df, 'WTMP', 'Water Temperature', 'C')
-    plot_graphs(df, 'WSPD', 'Wind Speed', 'm/s')
-    plot_graphs(df, 'PRES', 'Pressure', 'hPa')
-    plot_windrose(df)
-    df = load_data(r'../measurements/Excel Files/task 2/cloud cover/basin averages/western_basin_average.csv')
-    df.rename(columns={r'Cloud Cover (Fraction)': 'cloud'}, inplace=True)
-    df = df[df > 0].dropna()
-    plot_graphs(df, r'cloud', r'Cloud Cover (Fraction)', '-')
-    df = load_data(r'../measurements/Excel Files/task 2/precipitation/basin averages/western_basin_precipitation_average.csv')
-    plot_graphs(df, 'PRCP', 'Precipitation', 'mm/day')
-    df = load_data(r'../measurements/Excel Files/task 3/Maumee River/4192500_discharge.csv')
-    df.rename(columns={r'Inflow volume [m3 d-1]': 'inflow'}, inplace=True)
-    plot_graphs(df, r'inflow', r'Inflow volume', 'm3/d')
-    df = load_data(r'../measurements/Excel Files/task 3/Maumee River/4192500.csv', encoding="ISO-8859-1")
-    df.rename(columns={r'*Orthophosphate, water, filtered, as PO4 [mg m-3]': 'PO4'}, inplace=True)
-    plot_graphs(df, r'PO4', r'PO4', 'mg/m3', lines=True)
-    df.rename(columns={r'Inflow concentration as NH4 [mg m-3]': 'NH4'}, inplace=True)
-    plot_graphs(df, r'NH4', r'NH4', 'mg/m3', lines=True)
-    df.rename(columns={r'*Suspended sediment concentration [mg m-3]': 'Suspended'}, inplace=True)
-    plot_graphs(df, r'Suspended', r'Suspended sediment concentration', 'mg/m3', lines=True)
-    df = load_data(r'../measurements/Excel Files/task 3/Maumee River/4193490_temp.csv', encoding="ISO-8859-1")
-    df.rename(columns={r'Inflow temperature [°C]': 'inflowT'}, inplace=True)
-    df.rename(columns={r'Inflow concentration of O2 [mg m-3]': 'inflowO2'}, inplace=True)
-    df.rename(columns={r'Inflow pH [-]': 'inflowpH'}, inplace=True)
-    plot_graphs(df, r'inflowT', r'Inflow Temperature', 'C')
-    plot_graphs(df, r'inflowO2', r'Inflow O2', 'mg/m3', lines=True)
-    plot_graphs(df, r'inflowpH', r'Inflow pH', '-', lines=True)
-    df = load_data(r'../measurements/Excel Files/task 3/Maumee River/4193500_discharge.csv', encoding="ISO-8859-1")
-    df.rename(columns={r'Inflow volume [m3 d-1]': 'inflowV'}, inplace=True)
-    df.rename(columns={r'Suspended sediment concentration [mg m-3]': 'inflowSusp'}, inplace=True)
-    plot_graphs(df, r'inflowV', r'Inflow Volume', 'm3/d')
-    plot_graphs(df, r'inflowSusp', r'Inflow Suspended sediment', 'mg/m3', lines=True)
-    df = load_data(r'../measurements/Excel Files/task 3/Maumee River/4193500.csv', encoding="ISO-8859-1")
-    df.rename(columns={r'Inflow temperature [°C]': 'inflowT'}, inplace=True)
-    plot_graphs(df, r'inflowT', r'Inflow Temperature', 'C')
-    df.rename(columns={r'Inflow pH [-]': 'inflowpH'}, inplace=True)
-    plot_graphs(df, r'inflowpH', r'Inflow pH', '-', lines=True)
-    df.rename(columns={r'Suspended sediment concentration [mg m-3]': 'inflowSusp'}, inplace=True)
-    df.rename(columns={r'Inflow concentration of O2 [mg m-3]': 'inflowO2'}, inplace=True)
-    df.rename(columns={r'*Suspended solids, water, unfiltered [mg m-3]': 'inflowSuspUnfilt'}, inplace=True)
-    plot_graphs(df, r'inflowO2', r'Inflow O2', 'mg/m3')
-    # plot_graphs(df, r'inflowSuspUnfilt', r'Inflow Suspended solids, unfiltered', 'mg/m3')
-    df.rename(columns={r'*Phosphorus, water, unfiltered, as phosphorus [mg m-3]': 'inflowP'}, inplace=True)
-    df.inflowP[df.inflowP > 3000] = 0
-    plot_graphs(df, r'inflowP', r'Inflow P, unfiltered', 'mg/m3', lines=True)
-    df.rename(columns={r'* Phosphorus, water, filtered, as phosphorus [mg m-3]': 'inflowPfil'}, inplace=True)
-    df.rename(columns={r'* Orthophosphate, water, filtered,  as phosphorus [mg m-3]': 'inflowPO4filA'}, inplace=True)
-    df.rename(columns={r'Inflow concentration of dissolved organic carbon (DOC) [mg m-3]': 'inflowDOC'}, inplace=True)
-    df.rename(columns={r'Inflow concentration of dissolved inorganic carbon (DIC) [mg m-3]': 'inflowDIC'}, inplace=True)
-    df.rename(columns={r'Inflow concentration of Ca2+ [mg m-3]': 'inflowCa'}, inplace=True)
-    df.rename(columns={r'Inflow concentration of SO4 [mg m-3]': 'inflowSO4'}, inplace=True)
-    df.rename(columns={r'Inflow concentration of dissolved silica [mg m-3]': 'inflowDSi'}, inplace=True)
-    df.rename(columns={r'Inflow concentration of dissolved silica [mg m-3]': 'inflowDSi'}, inplace=True)
-    df.rename(columns={r'*Orthophosphate, water, filtered, as PO4 [mg m-3]': 'inflowPO4fil'}, inplace=True)
-    df.rename(columns={r'*Iron, suspended sediment, recoverable [mg m-3]': 'inflowFeSuspSed'}, inplace=True)
-    df.rename(columns={r'*Iron, water, unfiltered, recoverable [mg m-3]': 'inflowFe'}, inplace=True)
-    df.rename(columns={r'*Iron, water, filtered [mg m-3]': 'inflowFefil'}, inplace=True)
-    df.rename(columns={r'Inflow concentration of aluminum (Al3+) [mg m-3]': 'inflowAl'}, inplace=True)
-    df.rename(columns={r'Inflow concentration of chlorophyll-a (Chla-P) [mg m-3]': 'inflowChlP'}, inplace=True)
-    df.rename(columns={r'Inflow concentration of NH4 [mg m-3]': 'inflowNH4'}, inplace=True)
-    df.rename(columns={r'Inflow concentration of NO3 [mg m-3]': 'inflowNO3'}, inplace=True)
-    plot_graphs(df, r'inflowPO4filA', r'Inflow P, filtered', 'mg/m3', lines=True)
-    plot_graphs(df, r'inflowPfil', r'Inflow P, filtered', 'mg/m3', lines=True)
-    plot_graphs(df, r'inflowDOC', r'Inflow DOC', 'mg/m3', lines=True)
-    plot_graphs(df, r'inflowDIC', r'Inflow DIC', 'mg/m3', lines=True)
-    plot_graphs(df, r'inflowCa', r'Inflow Ca2+', 'mg/m3', lines=True)
-    plot_graphs(df, r'inflowSO4', r'Inflow SO4', 'mg/m3', lines=True)
-    df.inflowDSi[df.inflowDSi > 450000] = 0
-    plot_graphs(df, r'inflowDSi', r'Inflow dSi', 'mg/m3', lines=True)
-    plot_graphs(df, r'inflowPO4fil', r'Inflow PO4', 'mg/m3', lines=True)
-    plot_graphs(df, r'inflowFeSuspSed', r'Inflow Fe suspended sediments', 'mg/m3', lines=True)
-    plot_graphs(df, r'inflowFe', r'Inflow Fe', 'mg/m3', lines=True)
-    plot_graphs(df, r'inflowFefil', r'Inflow Fe filtered', 'mg/m3', lines=True)
-    plot_graphs(df, r'inflowAl', r'Inflow Al3+', 'mg/m3', lines=True)
-    plot_graphs(df, r'inflowChlP', r'Inflow chlorophyll-a (Chla-P)', 'mg/m3', lines=True)
-    plot_graphs(df, r'inflowNH4', r'Inflow NH4', 'mg/m3', lines=True)
-    plot_graphs(df, r'inflowNO3', r'Inflow NO3', 'mg/m3', lines=True)
-
-
 def plotting_basin_temperatures():
+    """Plots graphs of temperatures in 3 basins    """
     df = load_data('../measurements/weather/National_Buoy_Data_Center/basin_average/western_basin_average.csv')
     plot_graphs(df, 'ATMP', 'Air Temperature Western basin', 'C', style='k-')
     df = load_data('../measurements/weather/National_Buoy_Data_Center/basin_average/central_basin_average.csv')
@@ -187,23 +161,36 @@ def plotting_basin_temperatures():
     plot_graphs(df, 'ATMP', 'Air Temperature Eastern basin', 'C', style='k-')
 
 
-def plot_graphs_in_subplot(df, column, lgnd, units, style='.', color=sns.xkcd_rgb["black"], ax=None):
+def plot_graphs_in_subplot(df, column, lgnd, units, style='.', color=sns.xkcd_rgb["black"], ax=None, time_lim=None):
+    """Summary
+
+    Args:
+        df (TYPE): Description
+        column (TYPE): Description
+        lgnd (TYPE): Description
+        units (TYPE): Description
+        style (str, optional): Description
+        color (TYPE, optional): Description
+        ax (None, optional): Description
+        time_lim (None, optional): Description
+
+    Returns:
+        TYPE: Description
+    """
     if ax is None:
         ax = plt.gca()
 
     ax.set_ylabel(lgnd + ', ' + units)
     ax.grid(linestyle='-', linewidth=0.2)
-    period = [df.index.values[0], df.index.values[-1]]
-    ax.set_xlim(period)
+    if time_lim is None:
+        time_lim = [np.datetime64('1980-01-01T00:00:00.000000000'), np.datetime64('2016-12-31T00:00:00.000000000')]
+
+    ax.set_xlim(time_lim)
+
     try:
         ax.plot(df.index.values, df[column].values, style, color=color, lw=3)
-        df["TMP"] = df.index.values                # index is a DateTimeIndex
-        df = df[df.TMP.notnull()]                  # remove all NaT values
-        df.drop(["TMP"], axis=1, inplace=True)
-        df = df.sort_index()
-        df = df[period[0]:period[1]][np.isfinite(df[column])]
+        df = df[np.isfinite(df[column])]
         ax.ticklabel_format(style='sci', axis='y', scilimits=(0, 3))
-
         if df.size > 0:
             result = linear_fit(df, column)
             prstd, iv_l, iv_u = wls_prediction_std(result)
@@ -220,8 +207,16 @@ def plot_graphs_in_subplot(df, column, lgnd, units, style='.', color=sns.xkcd_rg
         ax.annotate(err, xy=(0.2, 0.5), xycoords='axes fraction', color='k', fontsize=10)
 
 
-
 def plot_subplots_for_river_inputs(basin, river):
+    """Summary
+
+    Args:
+        basin (TYPE): Description
+        river (TYPE): Description
+
+    Returns:
+        TYPE: Description
+    """
     # basin = 'Western'
     # river = 'maumeeriver'
     print('%s, %s' % (river.title(), basin))
@@ -229,7 +224,7 @@ def plot_subplots_for_river_inputs(basin, river):
     df = load_data(r'../measurements/Excel Files/task 3/' + basin + ' Basin/' + river + '_average.csv', encoding="ISO-8859-1")
 
     for clmn in df.columns:
-        df.rename(columns={clmn:clmn.strip().replace("*","")}, inplace=True)
+        df.rename(columns={clmn: clmn.strip().replace("*", "")}, inplace=True)
 
     df.rename(columns={r'Inflow volume [m3 d-1]': 'inflowQ'}, inplace=True)
     df.rename(columns={r'Inflow temperature [°C]': 'inflowT'}, inplace=True)
@@ -286,26 +281,93 @@ def plot_subplots_for_river_inputs(basin, river):
     plot_graphs_in_subplot(df, 'IronFilRec', 'Iron, filt., recov.', '$[mg$ $m^{-3}]$', ax=axes[3, 4])
     plot_graphs_in_subplot(df, 'dSI', 'Dissolved silica', '$[mg$ $m^{-3}]$', ax=axes[4, 4])
     plt.tight_layout(pad=0.2, w_pad=0.5, h_pad=0)
-    fig.savefig('plots/rivers/' + basin + ' basin/' + river + '.png', dpi=300)
+    fig.savefig('plots/rivers/' + basin + ' basin/' + river + '.png', dpi=DPI)
     plt.close()
     return df
 
 
-def parsing_folders_with_names():
+def load_matching_files_df(path, wildcard, skiprows=None):
+    """Summary
+
+    Args:
+        path (TYPE): Description
+        wildcard (TYPE): Description
+        skiprows (None, optional): Description
+
+    Returns:
+        TYPE: Description
+    """
+    all_files = glob.glob(os.path.join(path, wildcard + "*.csv"))     # advisable to use os.path.join as this makes concatenation OS independent
+    df_from_each_file = (load_data(f, skiprows=skiprows) for f in all_files)
+    concatenated_df = pd.concat(df_from_each_file, ignore_index=False)
+    return concatenated_df
+
+
+def plotting_weather_basin(basin):
+    """Summary
+
+    Args:
+        basin (TYPE): Description
+
+    Returns:
+        TYPE: Description
+    """
+    df = load_matching_files_df('../measurements/Excel Files/task 1/Basins averages/', basin)
+    df2 = load_matching_files_df('../measurements/Excel Files/task 2/cloud cover/basin averages/', basin)
+    df3 = load_matching_files_df('../measurements/Excel Files/task 2/precipitation/basin averages/', basin)
+    df4 = load_matching_files_df('../measurements/Excel Files/task 2/relative humidity, solar radiation + others/basin averages/', basin, skiprows=9)
+    df2.rename(columns={r'Cloud Cover (Fraction)': 'cloud'}, inplace=True)
+    df2 = df2[df2 > 0].dropna()
+    df3['TTL_PRCP'] = df3['SNOW'] + df3['PRCP']
+
+    plt.close()
+    fig, axes = plt.subplots(2, 4, sharex='col', figsize=(20, 10), dpi=150)
+    plot_graphs_in_subplot(df, 'ATMP', 'Temperature', '$[C]$', ax=axes[0, 0])
+    plot_graphs_in_subplot(df, 'WSPD', 'Wind Speed', '$[m$ $s^{-1}]$', ax=axes[0, 1])
+    plot_graphs_in_subplot(df, 'PRES', 'Pressure', '$[hPa]$', ax=axes[0, 2])
+    plot_graphs_in_subplot(df, 'WTMP', 'Water Temperature', 'C', ax=axes[0, 3])
+    plot_graphs_in_subplot(df2, r'cloud', r'Cloud Cover (Fraction)', '$[-]$', ax=axes[1, 0])
+    plot_graphs_in_subplot(df3, 'TTL_PRCP', 'Precipitation (Snow and Rain)', '$[mm$ $day^{-1}]$', ax=axes[1, 1])
+    plot_graphs_in_subplot(df4, 'SRAD', 'Solar Radiation', '$[MJ$ $m^{-2}$ $day^{-1}]$', ax=axes[1, 2])
+    plot_graphs_in_subplot(df4, 'RH2M', 'Relative Humidity', '$[\%]$', ax=axes[1, 3])
+    plt.tight_layout(pad=0.2, w_pad=0.5, h_pad=0)
+    if SAVE_FIG:
+        fig.savefig('plots/weather/' + basin + ' basin weather.png', dpi=DPI)
+    plt.show()
+    # plt.close()
+
+
+def plotting_river_input():
+    """dfs = list(plotting_river_input())
+    """
+
     for basin in ['Eastern', 'Central', 'Western']:
         files = os.listdir('../measurements/Excel Files/task 3/' + basin + ' Basin')
-        files.remove('Icon\r')
+        if 'Icon\r' in files:
+            files.remove('Icon\r')
+        if '.DS_Store' in files:
+            files.remove('.DS_Store')
         for river in files:
             # print(river[:-12])
-            yield plot_subplots_for_river_inputs(basin, river[:-12])
+            plot_subplots_for_river_inputs(basin, river[:-12])
+
+
+def plotting_weather():
+    for basin in ['eastern', 'central', 'western']:
+        plotting_weather_basin(basin)
+
 
 if __name__ == '__main__':
-    dfs = list(parsing_folders_with_names())
+    # plotting_weather_basin('eastern')
+    plotting_weather()
+    plotting_river_input()
+    # df3 = load_matching_files_df('../measurements/Excel Files/task 2/precipitation/basin averages/', 'eastern')
+    # df3['TTL_PRCP'] = df3['SNOW'] + df3['PRCP']
+    # plot_graphs_in_subplot(df3, 'TTL_PRCP', 'Precipitation (Snow and Rain)', '$[mm$ $day^{-1}]$', ax=None)
+    # plt.show()
+    # plt.close()
+    # df3 = load_matching_files_df('../measurements/Excel Files/task 2/precipitation/basin averages/', "western")
+    # df4 = load_matching_files_df('../measurements/Excel Files/task 2/relative humidity, solar radiation + others/basin averages/', "western", skiprows=9)
+    # df = load_matching_files_df('../measurements/Excel Files/task 1/Basins averages/', "western")
+    # dfs = list(parsing_folders_with_names())
     # df = list(parsing_folders_with_names())
-    # df = plot_subplots_for_river_inputs()
-    # df = load_data('../measurements/weather/National_Buoy_Data_Center/basin_average/western_basin_average.csv')
-    # plot_graphs(df, 'ATMP', 'Air Temperature Western basin', 'C', style='k-')
-    # plot_subplots_for_river_inputs()
-    # df = load_data(r'../measurements/Excel Files/task 3/Western Basin/maumeeriver_average.csv', encoding="ISO-8859-1")
-    # df.rename(columns={r'Inflow volume [m3 d-1]': 'inflowQ'}, inplace=True)
-    # plot_graphs(df, 'inflowQ', 'Inflow volume', '$[m^3$ $d^{-1}]$')
